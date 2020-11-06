67f0eaafba7b5be13aa48e0aaa295c4a9489e5ff
Laura <viglionigr@gmail.com>
2020-11-06 10:41:06 -0300

    feat: updated configs

--- src/helpers/logic.ts
+++ src/helpers/logic.ts
@@ -1,3 +1,27 @@
-import { compose, equals, not } from "ramda";
+import {
+  all,
+  always,
+  compose,
+  equals,
+  identity,
+  ifElse,
+  not,
+  unapply,
+} from 'ramda'
+import { ReactElement } from 'react'
 
-export const notEquals = (a: any, b: any) => compose(not, equals(a))(b)
+/*
+ * Basic logic
+ */
+
+export const notEquals = (a: unknown, b: unknown) => compose(not, equals(a))(b)
+
+export const allTruthy = unapply(all(identity))
+
+/*
+ * Returns component if condition
+ */
+export const showIf = (condition: boolean) => (
+  component: ReactElement,
+): ReactElement | null =>
+  ifElse(identity, always(component), always(null))(condition)
--- src/helpers/string.ts
+++ src/helpers/string.ts
@@ -1,19 +1,23 @@
-import { always, compose, concat, equals, ifElse, map, reduce, toString, unapply } from "ramda";
+import {
+  always,
+  compose,
+  concat,
+  equals,
+  ifElse,
+  map,
+  reduce,
+  toString,
+  unapply,
+} from 'ramda'
 
 export const isString = (element: unknown) => equals(typeof element, 'string')
 
-export const convertToString = (el: any) =>
-  ifElse(
-    isString,
-    always(el),
-    () => toString(el))
-    (el)
+export const convertToString = (el: unknown): string =>
+  ifElse(isString, always(el), () => toString(el))(el)
 
-export const concatAll =
-  unapply(
-    compose(
-      reduce<string, string>(concat, ''),
-      map<any, string>(convertToString)))
-
-
-
+export const concatAll = unapply(
+  compose(
+    reduce<string, string>(concat, ''),
+    map<unknown, string>(convertToString),
+  ),
+)
